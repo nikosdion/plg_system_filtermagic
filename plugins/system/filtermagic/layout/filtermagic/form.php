8c09af460a130f8526f3e4e97d54a81e152c2850
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-12-14 12:08:46 +0200

    Shape up the form layout

--- plugins/system/filtermagic/layout/filtermagic/form.php
+++ plugins/system/filtermagic/layout/filtermagic/form.php
@@ -8,6 +8,7 @@
 defined('_JEXEC') || die;
 
 use Joomla\CMS\Form\Form;
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\FileLayout;
 
 /**
@@ -28,7 +29,16 @@ if (empty($filters))
 <form name="filtermagic"
 	  action="<?= \Joomla\CMS\Uri\Uri::current() ?>"
 	  method="post"
-	  class="my-2 d-flex flex-row gap-2 align-items-center"
+	  class="my-2 d-flex flex-row gap-2 align-items-center bg-light border rounded border-1 p-2"
 >
+	<label class="visually-hidden">
+		<?= Text::_('JSEARCH_FILTER_LABEL') ?>
+	</label>
+
 	<?= $this->sublayout('fields', $displayData) ?>
+
+	<button type="submit" class="btn btn-primary">
+		<span class="fa fa-search" aria-hidden="true"></span>
+		<?= Text::_('JSEARCH_FILTER') ?>
+	</button>
 </form>
